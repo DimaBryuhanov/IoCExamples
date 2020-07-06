f8f3c97d098ec404ef27078335e7f119adddab87
dmitrii_bryuhanov@outlook.com <dmitrii_bryuhanov@outlook.com>
2020-07-06 15:20:48 +0300

    Added ProductService, FileLogger classes

---
+++ Models/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoCExamples.Models
+{
+    class ProductService
+    {
+        private readonly FileLogger _fileLogger = new FileLogger();
+        public void Log(string message)
+        {
+            _fileLogger.Log(message);
+        }
+    }
+}
