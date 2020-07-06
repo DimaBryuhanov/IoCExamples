fe97a3c8d3b2aded8d206c0f6f069f7c8fbb74e6
dmitrii_bryuhanov@outlook.com <dmitrii_bryuhanov@outlook.com>
2020-07-06 15:25:53 +0300

    added DatabaseLogger - an inflexible way...

--- Models/ProductService.cs
+++ Models/ProductService.cs
@@ -7,7 +7,13 @@ namespace IoCExamples.Models
     class ProductService
     {
         private readonly FileLogger _fileLogger = new FileLogger();
+        private readonly DatabaseLogger _databaseLogger =
+          new DatabaseLogger();
         public void Log(string message)
+        {
+            _fileLogger.Log(message);
+        }
+        public void LogToDatabase(string message)
         {
             _fileLogger.Log(message);
         }
