bf92df36cf4288a64b64c5a7e77fb83fb918bb5b
dmitrii_bryuhanov@outlook.com <dmitrii_bryuhanov@outlook.com>
2020-07-06 15:44:00 +0300

    IoC implemented

--- Models/ProductService.cs
+++ Models/ProductService.cs
@@ -7,10 +7,14 @@ namespace IoCExamples.Models
 {
     class ProductService
     {
+        private readonly ILogger _logger;
+        public ProductService(ILogger logger)
+        {
+            _logger = logger;
+        }
         public void Log(string message)
         {
-            ILogger logger = new FileLogger();
-            logger.Log(message);
+            _logger.Log(message);
         }
 
     }
